1496a0a88c1c0905e8b1fa3c13598cdc869be706
Alexander Esp <alexjesp@gmail.com>
2015-11-20 16:41:03 +0000

    Make content scripts more immutable

--- src/js/content/toolkit_experiment_previewer.js
+++ src/js/content/toolkit_experiment_previewer.js
@@ -1,231 +1,231 @@
 var $ = require('jquery')
 var ToolkitCreativeResolver = require('./toolkit_creative_resolver.js')()
 
-var experiment_previewer = {
-  initialised: false,
-  toolkit: null,
-  selectedExperiments: {},
+module.exports = function createExperimentPreviewer () {
+  var experiment_previewer = {
+    initialised: false,
+    toolkit: null,
+    selectedExperiments: {},
 
-  init: function (t) {
-    if (!t) return
-    var _self = this
-    this.toolkit = t
-    this.selectedExperiments = {}
+    init: function (t) {
+      if (!t) return
+      var _self = this
+      this.toolkit = t
+      this.selectedExperiments = {}
 
-    _self.buildHTML()
-    _self.displayRunningPreviews()
-    _self.bindEvents()
-    _self.initialised = true
-  },
+      _self.buildHTML()
+      _self.displayRunningPreviews()
+      _self.bindEvents()
+      _self.initialised = true
+    },
 
-  buildHTML: function (snippetArr) {
-    var $html = $(["<div id='DeliverPreviewer'>",
-      "<div class='snippetCont'>",
-      '<span>Preview Tests</span>',
-      "<div class='activePreviews'></div>",
-      "<div class='activator'>",
-      "<p class='activate'>Preview <span>0 Tests</span></p>",
-      "<p class='cancel'>Cancel</p>",
-      "<p class='clearPreview'>Clear Previews</p>",
-      '</div>',
-      '</div>',
-      '</div>'].join(''))
-
-    var $preview_meta = this.toolkit.$el.find('#DeliverToolbarLayerPreviews')
-    $preview_meta.append($html)
-    if (this.toolkit.smartserve_preview && this.toolkit.smartserve_preview === true) {
-      $preview_meta.addClass('preview_mode')
-    }
-
-    return this.toolkit.$el.find('#DeliverToolbarMeta')
-  },
-
-  renderExperiments: function (data) {
-    var _self = this
-    var $listWrapper = this.toolkit.$el.find('#DeliverToolbarLayers')
-    _self.selectedExperiments = {}
-    $listWrapper.empty()
-
-    $(this.toolkit.data).each(function (index) {
-      var odd = (index % 2 === 0) ? 'even' : 'odd'
-      var $layer = $([
-        "<div class='DT-Layer " + odd + "' data-id='" + this.id + "' >",
-        "<input type='checkbox' data-experiemntId='" + this.id + "' data-creativeId='' class='' id='DeliverPreviewerCheckbox-" + this.id + "' />",
-        '<div>',
-        "<label for='DeliverPreviewerCheckbox-" + this.id + "'><span>" + this.name + '</span></label>',
+    buildHTML: function (snippetArr) {
+      var $html = $(["<div id='DeliverPreviewer'>",
+        "<div class='snippetCont'>",
+        '<span>Preview Tests</span>',
+        "<div class='activePreviews'></div>",
+        "<div class='activator'>",
+        "<p class='activate'>Preview <span>0 Tests</span></p>",
+        "<p class='cancel'>Cancel</p>",
+        "<p class='clearPreview'>Clear Previews</p>",
         '</div>',
-        '</div>'
-      ].join(''))
-      $listWrapper.append($layer)
-    })
+        '</div>',
+        '</div>'].join(''))
 
-    // Events
-    _self.bindExperimentEvents()
+      var $preview_meta = this.toolkit.$el.find('#DeliverToolbarLayerPreviews')
+      $preview_meta.append($html)
+      if (this.toolkit.smartserve_preview && this.toolkit.smartserve_preview === true) {
+        $preview_meta.addClass('preview_mode')
+      }
 
-    // Open list
-    _self.toolkit.closeEperimentInfo()
-    $listWrapper.addClass('open')
-  },
+      return this.toolkit.$el.find('#DeliverToolbarMeta')
+    },
 
-  displayRunningPreviews: function () {
-    var _self = this
+    renderExperiments: function (data) {
+      var _self = this
+      var $listWrapper = this.toolkit.$el.find('#DeliverToolbarLayers')
+      _self.selectedExperiments = {}
+      $listWrapper.empty()
 
-    if (_self.toolkit.smartserve_preview === true &&
-      _self.toolkit.preview_creatives &&
-      _self.toolkit.preview_creatives.length > 0
-    ) {
-      var $display = _self.toolkit.$el.find('.activePreviews')
-      $display.html(_self.toolkit.preview_creatives.length + ' Previews active')
-      $display.addClass('active')
-    }
-  },
-
-  disablePreviewMode: function () {
-    var _self = this
-    var expired = new Date()
-    expired.setMinutes(expired.getMinutes() - 15)
-    expired = expired.toUTCString()
-
-    document.cookie = 'smartserve_preview=true; expires=-' + expired + '; path=/; domain=' + _self.toolkit.ss_domain
-    document.cookie = 'etcForceCreative=[]; expires=' + expired + '; path=/; domain=' + _self.toolkit.ss_domain
-
-    _self.reloadPage()
-  },
-
-  registerIdForPreview: function (e_id) {
-    this.selectedExperiments[e_id] = true
-  },
-
-  removeIdForPreview: function (e_id) {
-    this.selectedExperiments[e_id] = false
-  },
-
-  requestCreativeIds: function (experiments) {
-    var _self = this
-    var experiment_promises = []
-
-    $.each(experiments, function (e) {
-      var promise = ToolkitCreativeResolver.requestCreativesByEperimentId(this, function () {})
-      experiment_promises.push(promise)
-    })
-
-    Promise.all(experiment_promises).then(function (data) {
-      // update cookies
-      _self.toolkit.c.info('COOKIE CREATIVE ID DATA: ', data)
-
-      var creatives = []
-      $.each(data, function (index) {
-        var variant = this[1]
-        var id = variant.master_id
-        creatives.push(id)
+      $(this.toolkit.data).each(function (index) {
+        var odd = (index % 2 === 0) ? 'even' : 'odd'
+        var $layer = $([
+          "<div class='DT-Layer " + odd + "' data-id='" + this.id + "' >",
+          "<input type='checkbox' data-experiemntId='" + this.id + "' data-creativeId='' class='' id='DeliverPreviewerCheckbox-" + this.id + "' />",
+          '<div>',
+          "<label for='DeliverPreviewerCheckbox-" + this.id + "'><span>" + this.name + '</span></label>',
+          '</div>',
+          '</div>'
+        ].join(''))
+        $listWrapper.append($layer)
       })
 
-      // set cookies and refresh
-      _self.runPreviewCookies(creatives)
-    }, function (e) {
-      _self.toolkit.c.error('ERROR: Failed to retreive all Creative IDs: ', e)
-    })
-  },
+      // Events
+      _self.bindExperimentEvents()
 
-  runPreviewCookies: function (creatives) {
-    var _self = this
-    var creative_str = creatives.join(',')
-    var expires = new Date()
-    expires.setMinutes(expires.getMinutes() + 15)
-    expires = expires.toUTCString()
+      // Open list
+      _self.toolkit.closeEperimentInfo()
+      $listWrapper.addClass('open')
+    },
 
-    this.toolkit.c.info('CURRENT DOMAIN: ', _self.toolkit.ss_domain)
-    this.toolkit.c.info('CURRENT CREATIVES: ', creatives)
+    displayRunningPreviews: function () {
+      var _self = this
 
-    document.cookie = 'smartserve_preview=true; expires=' + expires + '; path=/; domain=' + _self.toolkit.ss_domain
-    document.cookie = 'etcForceCreative=[' + creative_str + ']; expires=' + expires + '; path=/; domain=' + _self.toolkit.ss_domain
-
-    _self.reloadPage()
-  },
-
-  reloadPage: function () {
-    setTimeout(function () {
-      window.location.reload()
-    }, 250)
-  },
-
-  getSelectedExperiments: function () {
-    var _self = this
-    var exp = []
-
-    for (var i in _self.selectedExperiments) {
-      if (_self.selectedExperiments.hasOwnProperty(i) && _self.selectedExperiments[i] === true) {
-        exp.push(i)
+      if (_self.toolkit.smartserve_preview === true &&
+        _self.toolkit.preview_creatives &&
+        _self.toolkit.preview_creatives.length > 0
+      ) {
+        var $display = _self.toolkit.$el.find('.activePreviews')
+        $display.html(_self.toolkit.preview_creatives.length + ' Previews active')
+        $display.addClass('active')
       }
-    }
-    return exp
-  },
+    },
 
-  bindExperimentEvents: function (experiments) {
-    var _self = this
-    var $layers = this.toolkit.$el.find('.DT-Layer')
+    disablePreviewMode: function () {
+      var _self = this
+      var expired = new Date()
+      expired.setMinutes(expired.getMinutes() - 15)
+      expired = expired.toUTCString()
 
-    $layers.find('input[type="checkbox"]').change(function (e) {
-      var $this = $(this)
-      var id = $this.attr('data-experiemntId')
+      document.cookie = 'smartserve_preview=true; expires=-' + expired + '; path=/; domain=' + _self.toolkit.ss_domain
+      document.cookie = 'etcForceCreative=[]; expires=' + expired + '; path=/; domain=' + _self.toolkit.ss_domain
 
-      if ($this.is(':not(:checked)')) {
-        _self.removeIdForPreview(id)
-      } else {
-        _self.registerIdForPreview(id)
+      _self.reloadPage()
+    },
+
+    registerIdForPreview: function (e_id) {
+      this.selectedExperiments[e_id] = true
+    },
+
+    removeIdForPreview: function (e_id) {
+      this.selectedExperiments[e_id] = false
+    },
+
+    requestCreativeIds: function (experiments) {
+      var _self = this
+      var experiment_promises = []
+
+      $.each(experiments, function (e) {
+        var promise = ToolkitCreativeResolver.requestCreativesByEperimentId(this, function () {})
+        experiment_promises.push(promise)
+      })
+
+      Promise.all(experiment_promises).then(function (data) {
+        // update cookies
+        _self.toolkit.c.info('COOKIE CREATIVE ID DATA: ', data)
+
+        var creatives = []
+        $.each(data, function (index) {
+          var variant = this[1]
+          var id = variant.master_id
+          creatives.push(id)
+        })
+
+        // set cookies and refresh
+        _self.runPreviewCookies(creatives)
+      }, function (e) {
+        _self.toolkit.c.error('ERROR: Failed to retreive all Creative IDs: ', e)
+      })
+    },
+
+    runPreviewCookies: function (creatives) {
+      var _self = this
+      var creative_str = creatives.join(',')
+      var expires = new Date()
+      expires.setMinutes(expires.getMinutes() + 15)
+      expires = expires.toUTCString()
+
+      this.toolkit.c.info('CURRENT DOMAIN: ', _self.toolkit.ss_domain)
+      this.toolkit.c.info('CURRENT CREATIVES: ', creatives)
+
+      document.cookie = 'smartserve_preview=true; expires=' + expires + '; path=/; domain=' + _self.toolkit.ss_domain
+      document.cookie = 'etcForceCreative=[' + creative_str + ']; expires=' + expires + '; path=/; domain=' + _self.toolkit.ss_domain
+
+      _self.reloadPage()
+    },
+
+    reloadPage: function () {
+      setTimeout(function () {
+        window.location.reload()
+      }, 250)
+    },
+
+    getSelectedExperiments: function () {
+      var _self = this
+      var exp = []
+
+      for (var i in _self.selectedExperiments) {
+        if (_self.selectedExperiments.hasOwnProperty(i) && _self.selectedExperiments[i] === true) {
+          exp.push(i)
+        }
       }
-      // Update UI
-      var exp = _self.getSelectedExperiments()
-      var suffix = (exp.length === 1) ? 'Test' : 'Tests'
-      _self.toolkit.$el.find('#DeliverPreviewer').find('.activate span').html(exp.length + ' ' + suffix)
-    })
-  },
+      return exp
+    },
 
-  bindEvents: function () {
-    var _self = this
-    var $previewer = this.toolkit.$el.find('#DeliverPreviewer')
-    var $activator = this.toolkit.$el.find('.activator')
+    bindExperimentEvents: function (experiments) {
+      var _self = this
+      var $layers = this.toolkit.$el.find('.DT-Layer')
 
-    $previewer.click(function (e) {
-      var $this = $(this)
-      var parent = _self.toolkit.$el.find('#DeliverToolbarLayerPreviews')
-      _self.toolkit.$el.find('#DeliverPreviewer .activate span').html('0 Tests')
+      $layers.find('input[type="checkbox"]').change(function (e) {
+        var $this = $(this)
+        var id = $this.attr('data-experiemntId')
 
-      $this.toggleClass('active')
-      parent.toggleClass('active')
+        if ($this.is(':not(:checked)')) {
+          _self.removeIdForPreview(id)
+        } else {
+          _self.registerIdForPreview(id)
+        }
+        // Update UI
+        var exp = _self.getSelectedExperiments()
+        var suffix = (exp.length === 1) ? 'Test' : 'Tests'
+        _self.toolkit.$el.find('#DeliverPreviewer').find('.activate span').html(exp.length + ' ' + suffix)
+      })
+    },
 
-      if ($this.hasClass('active')) {
-        _self.renderExperiments()
-      } else {
+    bindEvents: function () {
+      var _self = this
+      var $previewer = this.toolkit.$el.find('#DeliverPreviewer')
+      var $activator = this.toolkit.$el.find('.activator')
+
+      $previewer.click(function (e) {
+        var $this = $(this)
+        var parent = _self.toolkit.$el.find('#DeliverToolbarLayerPreviews')
+        _self.toolkit.$el.find('#DeliverPreviewer .activate span').html('0 Tests')
+
+        $this.toggleClass('active')
+        parent.toggleClass('active')
+
+        if ($this.hasClass('active')) {
+          _self.renderExperiments()
+        } else {
+          _self.toolkit.resetLayers()
+        }
+      })
+
+      $previewer.find('.clearPreview').click(function (e) {
+        e.stopPropagation()
+        _self.disablePreviewMode()
+      })
+
+      $activator.find('.activate').click(function (e) {
+        e.stopPropagation()
+        var exp = _self.getSelectedExperiments()
+        if (exp.length === 0) {
+          return // 0 selected previews
+        }
+
+        _self.requestCreativeIds(exp)
+        _self.toolkit.c.info('CURRENT SELECTED EXPERIMENTS: ', exp)
+      })
+
+      $activator.find('.cancel').click(function (e) {
+        e.stopPropagation()
         _self.toolkit.resetLayers()
-      }
-    })
+        _self.toolkit.$el.find('#DeliverPreviewer .activate span').html('0 Tests')
+      })
+    }
 
-    $previewer.find('.clearPreview').click(function (e) {
-      e.stopPropagation()
-      _self.disablePreviewMode()
-    })
-
-    $activator.find('.activate').click(function (e) {
-      e.stopPropagation()
-      var exp = _self.getSelectedExperiments()
-      if (exp.length === 0) {
-        return // 0 selected previews
-      }
-
-      _self.requestCreativeIds(exp)
-      _self.toolkit.c.info('CURRENT SELECTED EXPERIMENTS: ', exp)
-    })
-
-    $activator.find('.cancel').click(function (e) {
-      e.stopPropagation()
-      _self.toolkit.resetLayers()
-      _self.toolkit.$el.find('#DeliverPreviewer .activate span').html('0 Tests')
-    })
   }
 
-}
-
-module.exports = function () {
   return experiment_previewer
 }
